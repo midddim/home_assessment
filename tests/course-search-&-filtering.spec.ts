e0fc423a20855d1cfcbbda2980e46de527f532ad
Dobrynin Dmytro <ddobrynin@simulmedia.com>
2025-07-24 12:13:07 +0300

    no message

--- tests/course-search-&-filtering.spec.ts
+++ tests/course-search-&-filtering.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from './fixtures';
 
 test.describe("Home page", () => {
-  test('has title @63e5e689', {tag: '@smoke'}, async ({ homePage, page }) => {
+  test('has title @8ed45828', {tag: '@smoke'}, async ({ homePage, page }) => {
     await homePage.toBeDisplayed();
 
     await expect(page).toHaveTitle('Alison | Free Online Courses & Online Learning'); // TODO Replace text with variable
   });
 
-  test('search for courses on a specific topic @8bd4ee54', {tag: '@smoke'}, async ({ homePage, page }) => {
+  test('search for courses on a specific topic @2a2c9afc', {tag: '@smoke'}, async ({ homePage, page }) => {
     const searchQuery: string = 'javascript';
 
     await homePage.searchCoursesBy(searchQuery);
@@ -16,7 +16,7 @@ test.describe("Home page", () => {
     expect(page.url(), "URL should end with search query").toContain(`/courses?query=${searchQuery}`);
   });
 
-  test('search for courses on a specific topic with additional params @74813399', {tag: '@smoke'}, async ({ homePage, page }) => {
+  test('search for courses on a specific topic with additional params @5df7b779', {tag: '@smoke'}, async ({ homePage, page }) => {
     const searchQuery: string = 'javascript';
     const type: string = 'certificate';
 
@@ -26,7 +26,7 @@ test.describe("Home page", () => {
     expect(page.url(), "URL should end with search query").toContain(`/courses?type=${type}&query=${searchQuery}`);
   });
 
-  test('search for courses with "No Results Found" result @a8493af0', {tag: '@smoke'}, async ({ homePage, page }) => {
+  test('search for courses with "No Results Found" result @a0e65b8d', {tag: '@smoke'}, async ({ homePage, page }) => {
     const searchQuery: string = '12333221122311';
 
     await homePage.searchCoursesBy(searchQuery);
