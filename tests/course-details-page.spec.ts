75ad416a89ebc6d0988b102703ef408ab121ce3d
Dobrynin Dmytro <ddobrynin@simulmedia.com>
2025-07-24 15:21:55 +0300

    Update IDs

--- tests/course-details-page.spec.ts
+++ tests/course-details-page.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from './fixtures';
 
 test.describe('Course Details page', () => {
-  test('has title and rating @65ca87a7', async ({ courseDetailsPage }) => {
+  test('has title and rating', {tag: ['@smoke', '@65ca87a7']}, async ({ courseDetailsPage }) => {
     await courseDetailsPage.toBeDisplayed();
     await expect(courseDetailsPage.page).toHaveTitle(
       'Quality Assurance (QA) Techniques | Free Online Course | Alison'
     );
   });
 
-  test('search for courses on a specific topic @76ac3a7c', async ({ courseDetailsPage }) => {
+  test('search for courses on a specific topic', {tag: ['@smoke', '@76ac3a7c']}, async ({ courseDetailsPage }) => {
     await expect(courseDetailsPage.page.url()).toContain(`/course/quality-assurance-qa-techniques-and-methodologies`);
   });
 });
